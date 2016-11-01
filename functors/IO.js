fa2c6da2fdd93a5f015fd2ebaab53882a60b888e
roobina90 <roobina1990@gmail.com>
2016-11-01 18:26:45 +0100

    excercises functors

--- functors/IO.js
+++ functors/IO.js
@@ -2,17 +2,21 @@ var _ = require("ramda");
 
 
 var IO = function(f) {
-  this._value = f;
+  this.unsafePerformIO = f;
 };
+
+IO.prototype.map = function(f) {
+  return new IO(_.compose(f, this.unsafePerformIO));
+};
+
 
 IO.of = function(x) {
   return new IO(function() {
     return x;
   });
 };
+      
+      
 
-IO.prototype.map = function(f) {
-  return new IO(_.compose(f, this._value));
-};
 
 module.exports = IO;
